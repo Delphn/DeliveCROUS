6d9ebf355e8c0c0dacd0add5b8ef2943884d7aa2
EmmZak <emmanuelzakaryan@gmail.com>
2020-12-30 16:45:28 +0100

    creates Cart.vue

---
+++ src/views/Cart.vue
@@ -0,0 +1,22 @@
+<template>
+    <base-layout>
+        cart vue
+    </base-layout>
+</template>
+
+<script>
+import { IonGrid, IonRow, IonCol, IonImg, IonItem, IonCard, IonCardTitle, IonTitle, IonButton, IonInput} from '@ionic/vue';
+
+export default {
+    name: 'Cart',
+    data() {
+        return {
+            test: null
+        }
+    },
+    methods: {
+        test () {
+        }
+    }
+}
+</script>
