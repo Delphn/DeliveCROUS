3c41a0221c25043c472ecf6981c537b81037e346
EmmZak <emmanuelzakaryan@gmail.com>
2021-01-03 18:00:10 +0100

    adds methods to CheckBox

--- src/components/ChechBox.vue
+++ src/components/ChechBox.vue
@@ -1,12 +1,44 @@
 <template>
-    <input type="checkbox" v-model="isChecked">
+    <input type="checkbox" v-model="isChecked" @change="updateCart">
 </template>
 
 <script>
-export default {
+import {mapGetters } from 'vuex';
 
+export default {
+    props: ['dish'],
     data () {
-        isChecked: null
+        return {
+            isChecked: null
+        }
+    },
+    computed: {
+        ...mapGetters['cartItems']
+    },
+    methods: {
+        updateCart() {
+            if (this.isChecked) {
+                this.$store.commit('addToCart', this.dish)
+            }
+            else{
+                this.$store.commit('removeFromCart0, this.dish')
+            }
+        },
+        isInCart(item) {
+            const isDishInCart = this.cartItems.includes(item)
+            this.isChecked = isDishInCart
+            console.log('is this in cart: ', isDishInCart)
+
+            return isDishInCart
+        }
+    },
+    watch: {
+        cartItems(val) {
+            console.log('current state: ', val)
+        }
+    },
+    mounted() {
+        this.isChecked = this.cartItems.includes(this.dish)
     }
 }
 </script>
